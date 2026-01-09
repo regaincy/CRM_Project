a469ac802eaf77862f19c362f37e85bd95906ffe
AnithaMarimuthu <anithavillupuram2003@gmail.com>
2026-01-09 20:17:42 +0530

    Initial commit

--- CustomerRelationshipManagement/src/main/java/com/example/CustomerRelationshipManagement/CustomerRelationshipManagementApplication.java
+++ CustomerRelationshipManagement/src/main/java/com/example/CustomerRelationshipManagement/CustomerRelationshipManagementApplication.java
@@ -8,6 +8,7 @@ public class CustomerRelationshipManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerRelationshipManagementApplication.class, args);
+        System.out.println("new");
 	}
 
 }
